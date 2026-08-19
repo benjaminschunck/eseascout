b9188438f6834b7cbd510f50e8bac2a64f8fa8a1
benjaminschunck <schunckbenjamin@gmail.com>
2026-08-19 15:09:39 +0200

    Refactor get_upcoming_matches to return raw matches instead of processed upcoming matches

--- scout/faceit_client.py
+++ scout/faceit_client.py
@@ -6,24 +6,4 @@ def get_upcoming_matches(championship_id, team_id, api_key):
     )
     matches = response.json()["items"]
 
-    upcoming_matches = []
-
-    for match in matches:
-        faction1 = match["teams"]["faction1"]
-        faction2 = match["teams"]["faction2"]
-
-        if faction1["faction_id"] == team_id:
-            us, opponent = faction1, faction2
-        elif faction2["faction_id"] == team_id:
-            us, opponent = faction2, faction1
-        else:
-            continue
-
-        timestamp = match["scheduled_at"]
-        upcoming_matches.append({
-            "us": us["name"],
-            "opponent": opponent["name"],
-            "timestamp": timestamp
-        })
-
-    return upcoming_matches
+    return matches
